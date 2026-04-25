98ff8514b5862ae92c2561f94b46abc5a061ff2e
Omar4589 <omar4589@hotmail.com>
2026-04-25 12:21:08 -0400

    adds analytics for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import SchaetzlePage from './pages/SchaetzlePage';
 import ClaytonPage from './pages/ClaytonPage';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/schaetzle" element={<SchaetzlePage />} />
-      <Route path="/clayton" element={<ClaytonPage />} />
-      <Route path="*" element={<Navigate to="/schaetzle" replace />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/schaetzle" element={<SchaetzlePage />} />
+        <Route path="/clayton" element={<ClaytonPage />} />
+        <Route path="*" element={<Navigate to="/schaetzle" replace />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
